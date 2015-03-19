2feb6f1eed2c9a48f248a688d7490b39bec8c3df
tomgsmith99 <tomgsmith99@gmail.com>
2015-03-18 21:04:45 -0400

    reworked EditProfile

--- demos/enterprise/backplane/index.php
+++ demos/enterprise/backplane/index.php
@@ -1,68 +1,34 @@
 <?php
 
 /***************************************/
-$homeDirName = "JanrainDemoSites";
+$homeDir = "JanrainDemoSites";
 
-// Finds out where we are in the filesystem and fixes path to home
-$paths["home"] = strstr(getcwd(), $homeDirName, TRUE) . $homeDirName;
+// Finds out where we are in the filesystem and fixes path to fsHome
 
-include $paths["home"] . "/demoGenerator.php";
+$fsHome = strstr(getcwd(), $homeDir, TRUE) . $homeDir;
+
+include $fsHome . "/includes/includes.php";
+
 /***************************************/
 
-// You can uncomment any of these lines and override these values
+// This section is where you set all your parameters
+// 
+// To get a look at all available parameters, set the demo type and run this
+// page with the parameter mode=debug
+// 
+// Key elements you should set
+// 
+// typeOfDemo = engagement || enterprise || socialAjax || socialRedirect
+// 
+// $params["typeOfDemo"] = "engagement";
+//
+// title = title for the demo, enclosed in <title> tags
+// 
+// $params["title"] = "<title>Janrain Demo Site</title>";
 
-/**********  TYPE OF DEMO  **************/
-// "enterprise" // default, even if you don't enter a value
-// "socialAjax"
-// "socialRedirect"
+$params["title"] = "<title>Janrain Enterprise with Backplane</title>";
 
-$typeOfDemo = "enterprise";
+// End of section where you set parameters
 
-/******* Settings for all types of demos *************/
-// PAGE TITLE
-$configItems["title"] = "<title>Janrain Enterprise with Backplane</title>";
-
-// PAGE CONTENT
-// $configItems["content"] = "";
-// The default value for content is determined by $typeOfDemo
-
-/*************** ENTERPRISE DEMO SETTINGS ***************/
-// PATH TO janrain-init
-// $configItems["janrain-init"] = "<script src = '" . $webPath . "scripts/janrain-init.js'></script>";
-
-// PATH TO janrain-utils
-// $configItems["janrain-utils"] = "<script src = '" . $webPath . "scripts/janrain-utils.js'></script>";
-
-// PATH TO LINKS
-// $configItems["links"] = $defaultPath . "templates/links.html";
-
-// PATH TO WIDGET SCREENS
-// $configItems["widgetScreens"] = $defaultPath . "templates/widgetScreens.html";
-
-/*************** SOCIAL-AJAX *****************/
-
-// $configItems["socialLoginScript"] = $paths["templates"] . "/socialLogin.html";
-
-// $configItems["janrainSettings"] = $paths["default"] . "/scripts/social/ajax/settings.html";
-
-// $configItems["ajaxScript"] = $paths["default"] . "/scripts/social/ajax/ajax.php";
-
-// $configItems["errorChecking"] = $paths["default"] . "/scripts/social/ajax/errorChecking.html";
-
-// $configItems["jwol"] = $paths["default"] . "/scripts/social/ajax/jwol.html";
-
-/************** SOCIAL-REDIRECT **************/
-
-// PATH TO ANY OTHER SCRIPTS YOU WANT TO ADD
-// Scripts should all be in a single html file
-// enclosed by <script></script> tags.
-// Note: default value is empty. 
-// $configItems["otherScripts"] = "script.html";
-
-/*************** SOCIAL LOGIN SETTINGS ************/
-
-// $configItems["tokenURL"] = $paths["templates"] . "/tokenURL.php";
-
-// $configItems["tokenURL"] = "http://www.sampleTokenUrl.com/not-needed-for-client-side-validation";
-
-showPage($configItems, $typeOfDemo);
+// This command builds the demo and sends the html page to the browser
+showDemo($params, $fsHome);
