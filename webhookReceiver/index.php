ece3dc01007d6ab963e34c458e3012ca772f2fc4
tomgsmith99 <tomgsmith99@gmail.com>
2015-04-27 16:00:26 -0700

    webhook changes

--- webhookReceiver/index.php
+++ webhookReceiver/index.php
@@ -7,11 +7,17 @@ $logfileName = "webhook.txt";
 if (file_exists($logfileName)) {
     
     if (filesize($logfileName) > 10000) { $mode = "w+"; }
-    else { $mode = "r+"; }
+    else { $mode = "a+"; }
     
 }
 else { $mode = "w+"; }
 
 $filehandle = fopen($logfileName, $mode);
 
+$dateString = date(DATE_RFC822);
+
+fwrite($filehandle, "this page was loaded on " . $dateString . "\n");
+
 fwrite($filehandle, $postString);
+
+echo "<p>the date is: " . $dateString;
