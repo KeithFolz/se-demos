0c473464020ef6da4c86119164f0c424cc5ef68f
tomgsmith99 <tomgsmith99@gmail.com>
2015-04-28 12:25:26 -0700

    tweaks to webhooks ui

--- webhookReceiver/index.php
+++ webhookReceiver/index.php
@@ -1,12 +1,16 @@
 <?php
 
+// This gets the json from the webhook
 $postString = file_get_contents('php://input');
 
 $logfileName = "webhook.txt";
 
+// Check to see if the logfile exists
+// If it does, check its size. If it's more than 1 MB,
+// erase it and start over
 if (file_exists($logfileName)) {
     
-    if (filesize($logfileName) > 10000) { $mode = "w+"; }
+    if (filesize($logfileName) > 1000) { $mode = "w+"; }
     else { $mode = "a+"; }
     
 }
@@ -14,10 +18,11 @@ else { $mode = "w+"; }
 
 $filehandle = fopen($logfileName, $mode);
 
-$dateString = date(DATE_RFC822);
+// $dateString = date(DATE_RFC822);
 
-fwrite($filehandle, "this page was loaded on " . $dateString . "\n");
+// fwrite($filehandle, "this page was loaded on " . $dateString . "\n");
 
 fwrite($filehandle, $postString);
+fwrite($filehandle, "\n");
 
-echo "<p>the date is: " . $dateString;
+// echo "<p>the date is: " . $dateString;
--- webhookReceiver/viewer.php
+++ webhookReceiver/viewer.php
@@ -6,6 +6,18 @@ echo "<p>Here's what we've received recently from the webhook post: ";
 
 $logfileName = "webhook.txt";
 
-$content = "<p>" . file_get_contents($logfileName);
+$content = "";
+
+// $content = "<p>" . file_get_contents($logfileName);
+
+$lines = file($logfileName);
+
+for ($i = 1; $i <= 10; $i++) {
+    if (array_key_exists($i, $lines)) {
+        $content .= "<p>" . $lines[$i] . "</p>\n";
+    }
+    else { break; }
+    
+}
 
 echo $content;
