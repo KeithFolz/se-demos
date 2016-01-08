1e367cc65d1da44634a00452e941e89448cfb86f
rpropst <rpropst@janrain.com>
2016-01-08 14:27:37 -0800

    Creating progressive profile demo
    This demo requires:
    - jquery
    - bootstrap
    
    This version requires the user to click on a button to initiate the question.

---
+++ demos/enterprise/progressive_profile/config.php
@@ -0,0 +1,7 @@
+<?php
+define('JANRAIN_CAPTURE_API_URL', 'https://janrain-se-demo.eval.janraincapture.com');
+define('JANRAIN_LOGIN_CLIENT_ID', '73jsmdjy5gmgznv3yaw95uzppzgg9rsm');
+define('JANRAIN_LOGIN_CLIENT_SECRET', 'cbm43dg8ppx7vzr928tb7jqq98tqkvkk');
+define('JANRAIN_PASSWORD_RECOVER_URL', 'http://sedemo.local:8888/JanrainDemoSites/demos/enterprise/progressive_profile/index.html');
+
+
