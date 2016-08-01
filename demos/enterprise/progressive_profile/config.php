4c0a24e19d14ef085cb4251a18859ebc713be034
rpropst <rpropst@janrain.com>
2016-08-01 15:52:37 -0700

    Removing files that have already been moved to the JanrainDemoSites subdirectory

--- demos/enterprise/progressive_profile/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('JANRAIN_CAPTURE_API_URL', <YOUR_URL_HERE>);
-define('JANRAIN_LOGIN_CLIENT_ID', <YOUR_CLIENT_ID_HERE>);
-define('JANRAIN_LOGIN_CLIENT_SECRET', <YOUR_CLIENT_SECRET_HERE>);
-define('JANRAIN_PASSWORD_RECOVER_URL', 'http://sedemo.local:8888/JanrainDemoSites/demos/enterprise/progressive_profile/index.html');
-
-
